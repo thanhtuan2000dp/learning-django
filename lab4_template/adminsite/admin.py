4f5ddedd649dccde518d8004694527033e583bfa
davidtuan-macbook <thanhtuan2000dp@gmail.com>
2024-08-18 22:38:32 +0700

    done lab4

---
+++ lab4_template/adminsite/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Course, Instructor, Lesson
+
+# Register your models here.
+
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+
+class CourseAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'name', 'description']
+    inlines = [LessonInline]
+
+
+class InstructorAdmin(admin.ModelAdmin):
+    fields = ['user', 'full_time', 'total_learners']
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Instructor, InstructorAdmin)
